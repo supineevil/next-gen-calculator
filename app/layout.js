26eab935ee387e0fb48e21ebfdabc6dcedc50050
supineevil <75155924+supineevil@users.noreply.github.com>
2023-08-31 20:24:02 +0530

    few change

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,6 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* */}
+      <head>
+      <link rel="icon" href='/favicon.png' sizes="any" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
